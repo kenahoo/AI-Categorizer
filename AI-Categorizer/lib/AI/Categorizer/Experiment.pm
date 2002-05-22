f456e785df0dada4755f72c824d0e3e673853a19
ken <ken@a22099ce-342d-0410-a4cf-3bf2e08833ce>
2002-05-22 11:23:50 +0000

    Now computes micro stats properly.
    Gets the list of all categories from the hypothesis.
    
    Added the display_stats() method.

--- AI-Categorizer/lib/AI/Categorizer/Experiment.pm
+++ AI-Categorizer/lib/AI/Categorizer/Experiment.pm
@@ -21,58 +21,84 @@ use base qw(Class::Container);
 
 use AI::Categorizer::Util qw(intersection);
 
+use Params::Validate qw(:types);
+__PACKAGE__->valid_params
+  (
+#   categories => {type => ARRAYREF},
+  );
+
 sub new {
   my $package = shift;
   my $self = $package->SUPER::new(@_);
   $self->{$_} = 0 foreach qw(a b c d);
-  $self->{categories} = {};
   return $self;
 }
 
 sub add_hypothesis {
   my ($self, $hypothesis, $correct) = @_;
   my %assigned = map {($_, 1)} $hypothesis->categories;
-  
-  my %this;
-  $this{a} = intersection(\%assigned, $correct);
-  $this{b} = keys(%assigned) - $this{a};
-  $this{c} = @$correct - $this{a};
+
+  unless ($self->{categories}) {
+    my @all_cats = $hypothesis->all_categories;
+    $self->{categories} = { map {$_, {a=>0, b=>0, c=>0, d=>0}} @all_cats };
+  }
+  my $cats_table = $self->{categories};
+  $correct = UNIVERSAL::isa($correct, 'HASH') ? $correct : { map {($_ => 1)} @$correct };
 
   # Add to the macro tables
-  foreach (qw(a b c)) {
-    $self->{$_} += $this{$_};
+  {
+    my %this;
+    $this{a} = intersection(\%assigned, $correct);
+    $this{b} = keys(%assigned) - $this{a};
+    $this{c} = keys(%$correct) - $this{a};
+    $this{d} = keys(%$cats_table) - $this{a} - $this{b} - $this{c};
+    
+    foreach (qw(a b c d)) {
+      $self->{$_} += $this{$_};
+    }
   }
 
-  # Now this is the tricky part - add to the micro tables
-  foreach my $cat (@$correct) {
-#    if ($assigned{$cat}) {
-#      $self->{categories}{$cat}{a}++;
-#    } else {
-#      $self->{categories}{$cat}{c}++;
-#    }
+  # Add to the micro tables
+  foreach my $cat (keys %$cats_table) {
+    $cats_table->{$cat}{a}++ if  $assigned{$cat} and  $correct->{$cat};
+    $cats_table->{$cat}{b}++ if  $assigned{$cat} and !$correct->{$cat};
+    $cats_table->{$cat}{c}++ if !$assigned{$cat} and  $correct->{$cat};
+    $cats_table->{$cat}{d}++ if !$assigned{$cat} and !$correct->{$cat};
   }
+
+  $self->{hypotheses}++;
 }
 
-sub _compute {
+sub _invert {
   my ($self, $x, $y) = @_;
   return 1 unless $y;
   return 0 unless $x;
   return 1 / (1 + $y/$x);
 }
 
+sub macro_accuracy {
+  my $self = shift;
+  return +($self->{a} + $self->{d}) / ($self->{a} + $self->{b} + $self->{c} + $self->{d});
+}
+
+sub macro_error {
+  my $self = shift;
+  return +($self->{b} + $self->{c}) / ($self->{a} + $self->{b} + $self->{c} + $self->{d});
+}
+
 sub macro_precision {
   my ($self) = @_;
-  return $self->_compute($self->{a}, $self->{b});
+  return $self->_invert($self->{a}, $self->{b});
 }
 
 sub macro_recall {
   my $self = shift;
-  return $self->_compute($self->{a}, $self->{c});
+  return $self->_invert($self->{a}, $self->{c});
 }
 
 sub macro_F1 {
   my $self = shift;
-  return $self->_compute(2 * $self->{a}, $self->{b} + $self->{c});
+  return $self->_invert(2 * $self->{a}, $self->{b} + $self->{c});
 }
 
 sub micro_precision {
@@ -80,7 +106,7 @@ sub micro_precision {
   my $cats = $self->{categories};
   my $result;
   while (my ($cat, $scores) = each %$cats) {
-    $result += $self->_compute($scores->{a}, $scores->{b});
+    $result += $self->_invert($scores->{a}, $scores->{b});
   }
   return $result / keys %$cats;
 }
@@ -90,7 +116,7 @@ sub micro_recall {
   my $cats = $self->{categories};
   my $result;
   while (my ($cat, $scores) = each %$cats) {
-    $result += $self->_compute($scores->{a}, $scores->{c});
+    $result += $self->_invert($scores->{a}, $scores->{c});
   }
   return $result / keys %$cats;
 }
@@ -100,9 +126,50 @@ sub micro_F1 {
   my $cats = $self->{categories};
   my $result;
   while (my ($cat, $scores) = each %$cats) {
-    $result += $self->_compute(2 * $scores->{a}, $scores->{b} + $scores->{c});
+    $result += $self->_invert(2 * $scores->{a}, $scores->{b} + $scores->{c});
   }
   return $result / keys %$cats;
 }
+
+sub micro_accuracy {
+  my $self = shift;
+  my $cats = $self->{categories};
+  my $result;
+  while (my ($cat, $scores) = each %$cats) {
+    $result += ($scores->{a} + $scores->{d}) / $self->{hypotheses};
+  }
+  return $result / keys %$cats;
+}
+
+sub micro_error {
+  my $self = shift;
+  my $cats = $self->{categories};
+  my $result;
+  while (my ($cat, $scores) = each %$cats) {
+    $result += ($scores->{b} + $scores->{c}) / $self->{hypotheses};
+  }
+  return $result / keys %$cats;
+}
+
+sub display_stats {
+  my $self = shift;
+  
+  my $out = "+---------------------------------------------------------+\n";
+  $out   .= "|   miR    miP   miF1    miE     maR    maP   maF1    maE |\n";
+  $out   .= "| %.3f  %.3f  %.3f  %.3f   %.3f  %.3f  %.3f  %.3f |\n";
+  $out   .= "+---------------------------------------------------------+\n";
+
+  return sprintf($out,
+		 $self->micro_recall,
+		 $self->micro_precision,
+		 $self->micro_F1,
+		 $self->micro_error,
+		 $self->macro_recall,
+		 $self->macro_precision,
+		 $self->macro_F1,
+		 $self->macro_error,
+		);
+}
+
 
 1;

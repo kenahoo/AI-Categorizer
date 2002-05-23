0ceb05b94037118c01a4b719402676570eb260a2
ken <ken@a22099ce-342d-0410-a4cf-3bf2e08833ce>
2002-05-23 09:03:24 +0000

    Simplify the calculations for the macro tables

--- AI-Categorizer/lib/AI/Categorizer/Experiment.pm
+++ AI-Categorizer/lib/AI/Categorizer/Experiment.pm
@@ -19,8 +19,6 @@ use base qw(Class::Container);
 #         F1(a,0,0,d) = 1
 #         F1(0,+++,d) = 0
 
-use AI::Categorizer::Util qw(intersection);
-
 use Params::Validate qw(:types);
 __PACKAGE__->valid_params
   (
@@ -43,27 +41,16 @@ sub add_hypothesis {
     $self->{categories} = { map {$_, {a=>0, b=>0, c=>0, d=>0}} @all_cats };
   }
   my $cats_table = $self->{categories};
+
+  # Hashify
   $correct = UNIVERSAL::isa($correct, 'HASH') ? $correct : { map {($_ => 1)} @$correct };
 
-  # Add to the macro tables
-  {
-    my %this;
-    $this{a} = intersection(\%assigned, $correct);
-    $this{b} = keys(%assigned) - $this{a};
-    $this{c} = keys(%$correct) - $this{a};
-    $this{d} = keys(%$cats_table) - $this{a} - $this{b} - $this{c};
-    
-    foreach (qw(a b c d)) {
-      $self->{$_} += $this{$_};
-    }
-  }
-
-  # Add to the micro tables
+  # Add to the macro/micro tables
   foreach my $cat (keys %$cats_table) {
-    $cats_table->{$cat}{a}++ if  $assigned{$cat} and  $correct->{$cat};
-    $cats_table->{$cat}{b}++ if  $assigned{$cat} and !$correct->{$cat};
-    $cats_table->{$cat}{c}++ if !$assigned{$cat} and  $correct->{$cat};
-    $cats_table->{$cat}{d}++ if !$assigned{$cat} and !$correct->{$cat};
+    $cats_table->{$cat}{a}++, $self->{a}++ if  $assigned{$cat} and  $correct->{$cat};
+    $cats_table->{$cat}{b}++, $self->{b}++ if  $assigned{$cat} and !$correct->{$cat};
+    $cats_table->{$cat}{c}++, $self->{c}++ if !$assigned{$cat} and  $correct->{$cat};
+    $cats_table->{$cat}{d}++, $self->{d}++ if !$assigned{$cat} and !$correct->{$cat};
   }
 
   $self->{hypotheses}++;

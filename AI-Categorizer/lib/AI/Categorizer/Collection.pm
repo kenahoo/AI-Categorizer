9033bb4cef7a9ab2f78c9cf432cdabf411100b91
ken <ken@a22099ce-342d-0410-a4cf-3bf2e08833ce>
2002-06-03 04:44:08 +0000

    Add a 'verbose' parameter accepted

--- AI-Categorizer/lib/AI/Categorizer/Collection.pm
+++ AI-Categorizer/lib/AI/Categorizer/Collection.pm
@@ -1,8 +1,11 @@
 package AI::Categorizer::Collection;
 use strict;
 
+use Params::Validate qw(:types);
 use Class::Container;
 use base qw(Class::Container);
+__PACKAGE__->valid_params
+  ( verbose => {TYPE => SCALAR, default => 0} );
 
 # Abstract method
 sub next;

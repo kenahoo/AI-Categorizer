7b12c17b3cc0b09bd053996ae36245ed30dababc
ken <ken@a22099ce-342d-0410-a4cf-3bf2e08833ce>
2002-06-21 04:14:25 +0000

    Added category_hash and category_file parameters

--- AI-Categorizer/lib/AI/Categorizer/Collection.pm
+++ AI-Categorizer/lib/AI/Categorizer/Collection.pm
@@ -5,7 +5,28 @@ use Params::Validate qw(:types);
 use Class::Container;
 use base qw(Class::Container);
 __PACKAGE__->valid_params
-  ( verbose => {TYPE => SCALAR, default => 0} );
+  (
+   verbose => {type => SCALAR, default => 0},
+   category_hash => { type => HASHREF, default => {} },
+   category_file => { type => SCALAR, optional => 1 },
+  );
+
+sub new {
+  my $class = shift;
+  my $self = $class->SUPER::new(@_);
+
+  if ($self->{category_file}) {
+    local *FH;
+    open FH, $self->{category_file} or die "Can't open $self->{category_file}: $!";
+    while (<FH>) {
+      my ($doc, @cats) = split;
+      $self->{category_hash}{$doc} = \@cats;
+    }
+    close FH;
+  }
+
+  return $self;
+}
 
 # Abstract methods
 sub next;

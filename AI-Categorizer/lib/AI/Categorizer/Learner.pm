54b9d71a46fff48086e60eb9084fa10228124e73
ken <ken@a22099ce-342d-0410-a4cf-3bf2e08833ce>
2002-07-14 05:00:01 +0000

    Add the categorize_collection() method.
    Use a progress bar in the categorize_collection() method if verbose>=1.

--- AI-Categorizer/lib/AI/Categorizer/Learner.pm
+++ AI-Categorizer/lib/AI/Categorizer/Learner.pm
@@ -20,6 +20,10 @@ __PACKAGE__->contained_objects
 		  class => 'AI::Categorizer::Hypothesis',
 		  delayed => 1,
 		 },
+   experiment => {
+		  class => 'AI::Categorizer::Experiment',
+		  delayed => 1,
+		 },
   );
 
 # Subclasses must override these virtual methods:
@@ -51,7 +55,43 @@ sub train {
   $self->delayed_object_params('hypothesis',
 			       all_categories => [keys %{$self->{model}{cat_prob}}]
 			      );
+}
 
+sub prog_bar {
+  my ($self, $count) = @_;
+  
+  return sub { print STDERR '.' } unless eval "use Time::Progress; 1";
+  
+  my $pb = 'Time::Progress'->new;
+  $pb->attr(max => $count);
+  my $i = 0;
+  return sub {
+    $i++;
+    return if $i % 25;
+    my $string = '';
+    if (@_) {
+      my $e = shift;
+      $string = sprintf " (miF1=%.03f, maF1=%.03f)", $e->micro_F1, $e->macro_F1;
+    }
+    print STDERR $pb->report("%50b %p ($i/$count)$string\r", $i);
+    return $i;
+  };
+}
+
+sub categorize_collection {
+  my ($self, %args) = @_;
+  my $c = $args{collection} or die "No collection provided";
+  
+  my @all_cats = map $_->name, $self->categories;
+  my $experiment = $self->create_delayed_object('experiment', categories => \@all_cats);
+  my $pb = $self->verbose ? $self->prog_bar($c->count_documents) : sub {};
+  while (my $d = $c->next) {
+    my $h = $self->categorize($d);
+    $experiment->add_result([$h->categories], [map $_->name, $d->categories], $d->name);
+    $pb->($experiment);
+  }
+
+  return $experiment;
 }
 
 1;

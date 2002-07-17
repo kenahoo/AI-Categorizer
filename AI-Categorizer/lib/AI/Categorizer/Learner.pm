0acb83c91030d9ee9a0d1757a04ce3c83e60dcc5
ken <ken@a22099ce-342d-0410-a4cf-3bf2e08833ce>
2002-07-17 11:31:34 +0000

    Add documentation

--- AI-Categorizer/lib/AI/Categorizer/Learner.pm
+++ AI-Categorizer/lib/AI/Categorizer/Learner.pm
@@ -96,3 +96,120 @@ sub categorize_collection {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+AI::Categorizer::Learner - Abstract Machine Learner Class
+
+=head1 SYNOPSIS
+
+ use AI::Categorizer::Learner::NaiveBayes;  # Or other subclass
+ 
+ # Here $k is an AI::Categorizer::KnowledgeSet object
+ 
+ my $nb = new AI::Categorizer::Learner::NaiveBayes(...parameters...);
+ $nb->train(knowledge_set => $k);
+ $nb->save_state('filename');
+ 
+ ... time passes ...
+ 
+ $nb = AI::Categorizer::Learner::NaiveBayes->restore_state('filename');
+ my $c = new AI::Categorizer::Collection::Files( path => ... );
+ while (my $document = $c->next) {
+   my $hypothesis = $nb->categorize($document);
+   print "Best assigned category: ", $hypothesis->best_category, "\n";
+   print "All assigned categories: ", join(', ', $hypothesis->categories), "\n";
+ }
+
+=head1 DESCRIPTION
+
+The C<AI::Categorizer::Learner> class is an abstract class that will
+never actually be directly used in your code.  Instead, you will use a
+subclass like C<AI::Categorizer::Learner::NaiveBayes> which implements
+an actual machine learning algorithm.
+
+The general description of the Learner interface is documented here.
+
+=head1 METHODS
+
+=over 4
+
+=item new()
+
+Creates a new Learner and returns it.  Accepts the following
+parameters:
+
+=over 4
+
+=item knowledge_set
+
+A Knowledge Set that will be used by default during the C<train()>
+method.
+
+=item verbose
+
+If true, the Learner will display some diagnostic output while
+training and categorizing documents.
+
+=back
+
+=item train()
+
+=item train(knowledge_set => $k)
+
+Trains the categorizer.  This prepares it for later use in
+categorizing documents.  The C<knowledge_set> parameter must provide
+an object of the class C<AI::Categorizer::KnowledgeSet> (or a subclass
+thereof), populated with lots of documents and categories.  See
+L<AI::Categorizer::KnowledgeSet> for the details of how to create such
+an object.  If you provided a C<knowledge_set> parameter to C<new()>,
+specifying one here will override it.
+
+=item categorize($document)
+
+Returns an C<AI::Categorizer::Hypothesis> object representing the
+categorizer's "best guess" about which categories the given document
+should be assigned to.  See L<AI::Categorizer::Hypothesis> for more
+details on how to use this object.
+
+=item categorize_collection(collection => $collection)
+
+Categorizes every document in a collection and returns an Experiment
+object representing the results.
+
+=item knowledge_set()
+
+Gets/sets the internal C<knowledge_set> member.  Note that since the
+knowledge set may be enormous, some Learners may throw away their
+knowledge set after training or after restoring state from a file.
+
+=item $learner-E<gt>save_state($path)
+
+Saves the Learner for later use.  This method is inherited from
+C<AI::Categorizer::Storable>.
+
+=item $class-E<gt>restore_state($path)
+
+Returns a Learner saved in a file with C<save_state()>.  This method
+is inherited from C<AI::Categorizer::Storable>.
+
+=back
+
+=head1 AUTHOR
+
+Ken Williams, ken@forum.swarthmore.edu
+
+=head1 COPYRIGHT
+
+Copyright 2000-2001 Ken Williams.  All rights reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+AI::Categorize(3)
+
+=cut

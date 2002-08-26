e9bd83af3dbd3d0c685842fa17ad82bbdb2f40f2
ken <ken@a22099ce-342d-0410-a4cf-3bf2e08833ce>
2002-08-26 02:35:43 +0000

    Write documentation

--- AI-Categorizer/lib/AI/Categorizer/Learner/Weka.pm
+++ AI-Categorizer/lib/AI/Categorizer/Learner/Weka.pm
@@ -247,13 +247,13 @@ AI::Categorizer::Learner::Weka - Pass-through wrapper to Weka system
   
   # Here $k is an AI::Categorizer::KnowledgeSet object
   
-  my $nb = new AI::Categorizer::Learner::NaiveBayes(...parameters...);
+  my $nb = new AI::Categorizer::Learner::Weka(...parameters...);
   $nb->train(knowledge_set => $k);
   $nb->save_state('filename');
   
   ... time passes ...
   
-  $nb = AI::Categorizer::Learner::NaiveBayes->restore_state('filename');
+  $nb = AI::Categorizer::Learner->restore_state('filename');
   my $c = new AI::Categorizer::Collection::Files( path => ... );
   while (my $document = $c->next) {
     my $hypothesis = $nb->categorize($document);
@@ -263,10 +263,19 @@ AI::Categorizer::Learner::Weka - Pass-through wrapper to Weka system
 
 =head1 DESCRIPTION
 
-This is an implementation of the Naive Bayes decision-making
-algorithm, applied to the task of document categorization (as defined
-by the AI::Categorizer module).  See L<AI::Categorizer> for a complete
-description of the interface.
+This class doesn't implement any machine learners of its own, it
+merely passes the data through to the Weka machine learning system
+(http://www.cs.waikato.ac.nz/~ml/weka/).  This can give you access to
+a collection of machine learning algorithms not otherwise implemented
+in C<AI::Categorizer>.
+
+Currently this is a simple command-line wrapper that calls C<java>
+subprocesses.  In the future this may be converted to an
+C<Inline::Java> wrapper for better performance (shorter running
+times).  However, if you're looking for really great performance,
+you're probably looking in the wrong place - this Weka wrapper is
+intended more as a way to try lots of different machine learning
+methods.
 
 =head1 METHODS
 
@@ -275,25 +284,42 @@ of its methods are available unless explicitly mentioned here.
 
 =head2 new()
 
-Creates a new Naive Bayes Learner and returns it.  In addition to the
+Creates a new Weka Learner and returns it.  In addition to the
 parameters accepted by the C<AI::Categorizer::Learner> class, the
-Naive Bayes subclass accepts the following parameters:
+Weka subclass accepts the following parameters:
 
 =over 4
 
-=item * threshold
+=item * java_path
 
-Sets the score threshold for category membership.  The default is
-currently 0.3.  Set the threshold lower to assign more categories per
-document, set it higher to assign fewer.  This can be an effective way
-to trade of between precision and recall.
+Specifies where the C<java> executable can be found on this system.
+The default is simply C<java>, meaning that it will search your
+C<PATH> to find java.
+
+=item * weka_path
+
+Specifies the path to the C<weka.jar> file containing the Weka
+bytecode.  If Weka has been installed somewhere in your java
+C<CLASSPATH>, you needn't specify a C<weka_path>.
+
+=item * java_args
+
+Specifies a list of any additional arguments to give to the java
+process.  Commonly it's necessary to allocate more memory than the
+default, using an argument like C<-Xmx130MB>.
+
+=item * weka_classifier
+
+Specifies the Weka class to use for a categorizer.  The default is
+C<weka.classifiers.NaiveBayes>.  Consult your Weka documentation for a
+list of other classifiers available.
+
+=item * tmpdir
+
+A directory in which temporary files will be written when training the
+categorizer and categorizing new documents.  The default is C</tmp>.
 
 =back
-
-=head2 threshold()
-
-Returns the current threshold value.  With an optional numeric
-argument, you may set the threshold.
 
 =head2 train(knowledge_set => $k)
 
@@ -316,95 +342,13 @@ details on how to use this object.
 Saves the categorizer for later use.  This method is inherited from
 C<AI::Categorizer::Storable>.
 
-=head1 THEORY
-
-Bayes' Theorem is a way of inverting a conditional probability. It
-states:
-
-                P(y|x) P(x)
-      P(x|y) = -------------
-                   P(y)
-
-The notation C<P(x|y)> means "the probability of C<x> given C<y>."  See also
-L<"http://forum.swarthmore.edu/dr.math/problems/battisfore.03.22.99.html">
-for a simple but complete example of Bayes' Theorem.
-
-In this case, we want to know the probability of a given category given a
-certain string of words in a document, so we have:
-
-                    P(words | cat) P(cat)
-  P(cat | words) = --------------------
-                           P(words)
-
-We have applied Bayes' Theorem because C<P(cat | words)> is a difficult
-quantity to compute directly, but C<P(words | cat)> and C<P(cat)> are accessible
-(see below).
-
-The greater the expression above, the greater the probability that the given
-document belongs to the given category.  So we want to find the maximum
-value.  We write this as
-
-                                 P(words | cat) P(cat)
-  Best category =   ArgMax      -----------------------
-                   cat in cats          P(words)
-
-
-Since C<P(words)> doesn't change over the range of categories, we can get rid
-of it.  That's good, because we didn't want to have to compute these values
-anyway.  So our new formula is:
-
-  Best category =   ArgMax      P(words | cat) P(cat)
-                   cat in cats
-
-Finally, we note that if C<w1, w2, ... wn> are the words in the document,
-then this expression is equivalent to:
-
-  Best category =   ArgMax      P(w1|cat)*P(w2|cat)*...*P(wn|cat)*P(cat)
-                   cat in cats
-
-That's the formula I use in my document categorization code.  The last
-step is the only non-rigorous one in the derivation, and this is the
-"naive" part of the Naive Bayes technique.  It assumes that the
-probability of each word appearing in a document is unaffected by the
-presence or absence of each other word in the document.  We assume
-this even though we know this isn't true: for example, the word
-"iodized" is far more likely to appear in a document that contains the
-word "salt" than it is to appear in a document that contains the word
-"subroutine".  Luckily, as it turns out, making this assumption even
-when it isn't true may have little effect on our results, as the
-following paper by Pedro Domingos argues:
-L<"http://www.cs.washington.edu/homes/pedrod/mlj97.ps.gz">
-
-=head1 CALCULATIONS
-
-The various probabilities used in the above calculations are found
-directly from the training documents.  For instance, if there are 5000
-total tokens (words) in the "sports" training documents and 200 of
-them are the word "curling", then C<P(curling|sports) = 200/5000 =
-0.04> .  If there are 10,000 total tokens in the training corpus and
-5,000 of them are in documents belonging to the category "sports",
-then C<P(sports)> = 5,000/10,000 = 0.5> .
-
-Because the probabilities involved are often very small and we
-multiply many of them together, the result is often a tiny tiny
-number.  This could pose problems of floating-point underflow, so
-instead of working with the actual probabilities we work with the
-logarithms of the probabilities.  This also speeds up various
-calculations in the C<categorize()> method.
-
-=head1 TO DO
-
-More work on the confidence scores - right now the winning category
-tends to dominate the scores overwhelmingly, when the scores should
-probably be more evenly distributed.
-
 =head1 AUTHOR
 
 Ken Williams, ken@forum.swarthmore.edu
 
 =head1 COPYRIGHT
 
-Copyright 2000-2001 Ken Williams.  All rights reserved.
+Copyright 2000-2002 Ken Williams.  All rights reserved.
 
 This library is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
@@ -412,15 +356,5 @@ modify it under the same terms as Perl itself.
 =head1 SEE ALSO
 
 AI::Categorize(3)
-
-"A re-examination of text categorization methods" by Yiming Yang
-L<http://www.cs.cmu.edu/~yiming/publications.html>
-
-"On the Optimality of the Simple Bayesian Classifier under Zero-One
-Loss" by Pedro Domingos
-L<"http://www.cs.washington.edu/homes/pedrod/mlj97.ps.gz">
-
-A simple but complete example of Bayes' Theorem from Dr. Math
-L<"http://www.mathforum.com/dr.math/problems/battisfore.03.22.99.html">
 
 =cut

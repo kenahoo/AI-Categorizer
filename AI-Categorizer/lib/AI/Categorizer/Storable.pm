52ae3d76937da25b342b77b6bbfc1c371920b704
ken <ken@a22099ce-342d-0410-a4cf-3bf2e08833ce>
2002-09-20 04:29:51 +0000

    Store to a directory, not a file.  This makes it easier for subclasses to create stuff inside the directory.

--- AI-Categorizer/lib/AI/Categorizer/Storable.pm
+++ AI-Categorizer/lib/AI/Categorizer/Storable.pm
@@ -2,15 +2,21 @@ package AI::Categorizer::Storable;
 
 use strict;
 use Storable;
+use File::Spec ();
+use File::Path ();
 
 sub save_state {
-  my ($self, $file) = @_;
-  Storable::nstore($self, $file);
+  my ($self, $path) = @_;
+  if (-e $path) {
+    File::Path::rmtree($path) or die "Couldn't overwrite $path: $!";
+  }
+  mkdir($path, 0777) or die "Can't create $path: $!";
+  Storable::nstore($self, File::Spec->catfile($path, 'self'));
 }
 
 sub restore_state {
-  my ($package, $file) = @_;
-  return Storable::retrieve($file);
+  my ($package, $path) = @_;
+  return Storable::retrieve(File::Spec->catfile($path, 'self'));
 }
 
 1;

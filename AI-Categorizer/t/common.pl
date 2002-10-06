edba98fdc8b9cace57430b21245afbb330861b8d
kwilliams <kwilliams@a22099ce-342d-0410-a4cf-3bf2e08833ce>
2002-10-06 05:07:47 +0000

    Check the $learner->save_state() method to make sure the restored learner can categorize correctly

--- AI-Categorizer/t/common.pl
+++ AI-Categorizer/t/common.pl
@@ -1,6 +1,8 @@
 
 use strict;
 use Test;
+use AI::Categorizer;
+use AI::Categorizer::KnowledgeSet;
 
 sub have_module {
   my $module = shift;
@@ -51,4 +53,45 @@ sub run_test_docs {
   ok($r->best_category, 'vampire');
 
 }
+
+sub perform_standard_tests {
+  my %params = @_;
+  my $c = new AI::Categorizer(
+			      knowledge_set => AI::Categorizer::KnowledgeSet->new
+			      (
+			       name => 'Vampires/Farmers',
+			       stopwords => [qw(are be in of and)],
+			      ), 
+			      %params,
+			     );
+  ok($c);
+  
+  my %docs = training_docs();
+  while (my ($name, $data) = each %docs) {
+    $c->knowledge_set->make_document(name => $name, %$data);
+  }
+
+  my $l = $c->learner;
+  ok $l;
+  
+  if ($params{learner_class}) {
+    ok ref $l, $params{learner_class};
+  } else {
+    ok 1;
+  }
+
+  $l->train;
+  
+  run_test_docs($l);
+
+  # Make sure we can save state & restore state
+  $l->save_state('t/state');
+  $l = $l->restore_state('t/state');
+  ok $l;
+
+  run_test_docs($l);
+}
+
+sub num_standard_tests () { 8 }
+
 1;

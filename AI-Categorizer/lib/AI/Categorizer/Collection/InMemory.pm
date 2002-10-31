a0eb33a30ec40e71944ee65a29adc2a61c2673c7
kwilliams <kwilliams@a22099ce-342d-0410-a4cf-3bf2e08833ce>
2002-10-31 06:53:56 +0000

    New class for in-memory documents.  Useful for testing stuff.

---
+++ AI-Categorizer/lib/AI/Categorizer/Collection/InMemory.pm
@@ -0,0 +1,36 @@
+package AI::Categorizer::Collection::InMemory;
+use strict;
+
+use AI::Categorizer::Collection;
+use base qw(AI::Categorizer::Collection);
+
+use Params::Validate qw(:types);
+
+__PACKAGE__->valid_params
+  (
+   data => { type => HASHREF },
+  );
+
+sub next {
+  my $self = shift;
+  my ($name, $params) = each %{$self->{data}} or return;
+  foreach (@{$params->{categories}}) {
+    next if ref $_;
+    $_ = AI::Categorizer::Category->by_name(name => $_);
+  }
+
+  return AI::Categorizer::Document->new(name => $name, %$params);
+}
+
+sub rewind {
+  my $self = shift;
+  scalar keys %{$self->{data}};
+  return;
+}
+
+sub count_documents {
+  my $self = shift;
+  return scalar keys %{$self->{data}};
+}
+
+1;

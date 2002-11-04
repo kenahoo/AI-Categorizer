61c1d1200a87ea2d8ea1c6783b916890d9aa99a9
kwilliams <kwilliams@a22099ce-342d-0410-a4cf-3bf2e08833ce>
2002-11-04 05:54:25 +0000

    Add docs

--- AI-Categorizer/lib/AI/Categorizer/Learner/SVM.pm
+++ AI-Categorizer/lib/AI/Categorizer/Learner/SVM.pm
@@ -101,27 +101,87 @@ __END__
 
 =head1 NAME
 
-AI::Categorizer::Learner::DecisionTree - Perl extension for blah blah blah
+AI::Categorizer::Learner::SVM - Support Vector Machine Learner
 
 =head1 SYNOPSIS
 
-  use AI::Categorizer::Learner::DecisionTree;
-  blah blah blah
+  use AI::Categorizer::Learner::SVM;
+  
+  # Here $k is an AI::Categorizer::KnowledgeSet object
+  
+  my $l = new AI::Categorizer::Learner::SVM(...parameters...);
+  $l->train(knowledge_set => $k);
+  $l->save_state('filename');
+  
+  ... time passes ...
+  
+  $l = AI::Categorizer::Learner->restore_state('filename');
+  while (my $document = ... ) {  # An AI::Categorizer::Document object
+    my $hypothesis = $l->categorize($document);
+    print "Best assigned category: ", $hypothesis->best_category, "\n";
+  }
 
 =head1 DESCRIPTION
 
-Stub documentation for AI::Categorizer::Learner::DecisionTree, created
-by h2xs. It looks like the author of the extension was negligent
-enough to leave the stub unedited.
+This class implements a Support Vector Machine machine learner, using
+Cory Spencer's C<Algorithm::SVM> module.  In lots of the recent
+academic literature, SVMs perform very well for text categorization.
 
-Blah blah blah.
+=head1 METHODS
+
+This class inherits from the C<AI::Categorizer::Learner> class, so all
+of its methods are available unless explicitly mentioned here.
+
+=head2 new()
+
+Creates a new SVM Learner and returns it.  In addition to the
+parameters accepted by the C<AI::Categorizer::Learner> class, the
+SVM subclass accepts the following parameters:
+
+=over 4
+
+=item svm_kernel
+
+Specifies what type of kernel should be used when building the SVM.
+Default is 'linear'.  Possible values are 'linear', 'polynomial',
+'radial' and 'sigmoid'.
+
+=back
+
+=head2 train(knowledge_set => $k)
+
+Trains the categorizer.  This prepares it for later use in
+categorizing documents.  The C<knowledge_set> parameter must provide
+an object of the class C<AI::Categorizer::KnowledgeSet> (or a subclass
+thereof), populated with lots of documents and categories.  See
+L<AI::Categorizer::KnowledgeSet> for the details of how to create such
+an object.
+
+=head2 categorize($document)
+
+Returns an C<AI::Categorizer::Hypothesis> object representing the
+categorizer's "best guess" about which categories the given document
+should be assigned to.  See L<AI::Categorizer::Hypothesis> for more
+details on how to use this object.
+
+=head2 save_state($path)
+
+Saves the categorizer for later use.  This method is inherited from
+C<AI::Categorizer::Storable>.
 
 =head1 AUTHOR
 
-Ken Williams, <ken@mathforum.org>
+Ken Williams, ken@mathforum.org
+
+=head1 COPYRIGHT
+
+Copyright 2000-2002 Ken Williams.  All rights reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
-AI::Categorizer, AI::DecisionTree
+AI::Categorizer(3), Algorithm::SVM(3)
 
 =cut

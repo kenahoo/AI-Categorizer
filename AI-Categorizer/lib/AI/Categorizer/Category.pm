9d9bbce49474ca00ac2ec78c03b5676e551c6180
kwilliams <kwilliams@a22099ce-342d-0410-a4cf-3bf2e08833ce>
2002-11-06 01:19:56 +0000

    Add docs

--- AI-Categorizer/lib/AI/Categorizer/Category.pm
+++ AI-Categorizer/lib/AI/Categorizer/Category.pm
@@ -77,3 +77,91 @@ sub features {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+AI::Categorizer::Category - A named category of documents
+
+=head1 SYNOPSIS
+
+  my $category = AI::Categorizer::Category->by_name("sports");
+  my $name = $category->name;
+  
+  my @docs = $category->documents;
+  my $num_docs = $category->documents;
+  my $features = $category->features;
+  
+  $category->add_document($doc);
+  if ($category->contains_document($doc)) { ...
+
+=head1 DESCRIPTION
+
+This simple class represents a named category which may contain zero
+or more documents.  Each category is a "singleton" by name, so two
+Category objects with the same name should not be created at once.
+
+=head1 METHODS
+
+=over 4
+
+=item new()
+
+Creates a new Category object and returns it.  Accepts the following
+parameters:
+
+=over 4
+
+=item name
+
+The name of this category
+
+=item documents
+
+A reference to an array of Document objects that should belong to this
+category.
+
+=back
+
+=item by_name(name => $string)
+
+Returns the Category object with the given name, or creates one if no
+such object exists.
+
+=item documents()
+
+Returns a list of the Document objects in this category in a list
+context, or the number of such objects in a scalar context.
+
+=item features()
+
+Returns a FeatureVector object representing the sum of all the
+FeatureVectors of the Documents in this Category.
+
+=item add_document($document)
+
+Informs the Category that the given Document belongs to it.
+
+=item contains_document($document)
+
+Returns true if the given document belongs to this category, or false
+otherwise.
+
+=back
+
+=head1 AUTHOR
+
+Ken Williams, ken@mathforum.org
+
+=head1 COPYRIGHT
+
+Copyright 2000-2002 Ken Williams.  All rights reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+AI::Categorizer(3), Storable(3)
+
+=cut

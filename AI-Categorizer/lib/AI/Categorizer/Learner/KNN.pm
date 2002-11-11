10daa816ca893d0dab9bd4157f9639a166b4deca
kwilliams <kwilliams@a22099ce-342d-0410-a4cf-3bf2e08833ce>
2002-11-11 03:54:55 +0000

    The create_model() method doesn't need to do anything.
    Added a threshold() method.
    
    Removed inner loop in get_scores() method - just test the document
    hash directly.
    
    Inherit categorize() and categories() methods from superclass.
    
    Fix docs to be about KNN.
    
    Add credits for David Bell.

--- AI-Categorizer/lib/AI/Categorizer/Learner/KNN.pm
+++ AI-Categorizer/lib/AI/Categorizer/Learner/KNN.pm
@@ -12,18 +12,14 @@ __PACKAGE__->valid_params
    k_value => {type => SCALAR, default =>5},
   );
 
-sub create_model {
+# Don't need to do anything (but some initialization might be nice)
+sub create_model {}
+
+sub threshold {
   my $self = shift;
-  my $m = $self->{model} = {};
-  $m->{categories} = $self->knowledge_set->categories;
-  my $totaldocs = $self->knowledge_set->documents;
-  $m->{features} = $self->knowledge_set->features;
-  $m->{vocab_size} = $self->knowledge_set->features->length;
-  $m->{total_tokens} = $self->knowledge_set->features->sum;
-  $self->{model} = $m;
-
+  $self->{threshold} = shift if @_;
+  return $self->{threshold};
 }
-
 
 #--- returns scores of this document with k closest neighbours
 my $index =-1;
@@ -66,19 +62,14 @@ sub get_scores {
       $dscores[$i]=-1;
   }
   foreach my $doc(@docs){ # each doc in corpus 
-     # print "comparing to:";
-     # print $doc->name;
-     # print " \n";
+     warn "comparing to: ", $doc->name, "\n" if $self->verbose;
 	my $score=0;
-	foreach my $feature(keys %$features){ #for each feature in new doc
-	    my $doc_features = $doc->features->normalize->as_hash;
-		foreach my $f(keys (%$doc_features)){ #for each feature in doc
-			if (exists $features->{$f}){ #if matching feature
-			   # print "match on feature: $f \n";
-				$score = $score+$features->{$feature}*$doc_features->{$f}; #add to dot product
-			}
-		}
+	my $doc_features = $doc->features->normalize->as_hash;
+	foreach my $feature (keys %$features){ #for each feature in new doc
+	  next unless exists $doc_features->{$feature};
+	  $score += $features->{$feature} * $doc_features->{$feature}; #add to dot product
 	}
+	warn "Score for ", $doc->name, " (", ($doc->categories)[0]->name, ") is $score" if $self->verbose > 1;
         
 	#print "adding score: $score\n";
 	@dscores = subIn($score,@dscores);
@@ -125,33 +116,8 @@ sub get_scores {
   
  #print "scores: @{[ %scores ]}\n";
       my $t = $self->{threshold};
-  print "threshold = $t\n" ;
-  return \%scores;
-}
-
-sub categorize {
-  my ($self, $doc) = @_;
-
-  my $scores = $self->get_scores($doc);
-  
-  if ($self->verbose > 2) {
-    warn "scores: @{[ %$scores ]}" if $self->verbose > 3;
-
-    foreach my $key (sort {$scores->{$b} <=> $scores->{$a}} keys %$scores) {
-      print "$key: $scores->{$key}\n";
-    }
-  }
-
-  return $self->create_delayed_object('hypothesis',
-				      scores => $scores,
-				      threshold => $self->{threshold},
-				      document_name => $doc->name,
-				     );
-}
-
-sub categories {
-  my $self = shift;
-  return $self->knowledge_set->categories;
+  #print "threshold = $t\n" ;
+  return (\%scores, $self->{threshold});
 }
 
 1;
@@ -164,7 +130,7 @@ AI::Categorizer::Learner::KNN - K Nearest Neighbour Algorithm For AI::Categorize
 
 =head1 SYNOPSIS
 
-  use AI::Categorizer::Learner::NaiveBayes;
+  use AI::Categorizer::Learner::KNN;
   
   # Here $k is an AI::Categorizer::KnowledgeSet object
   
@@ -174,17 +140,17 @@ AI::Categorizer::Learner::KNN - K Nearest Neighbour Algorithm For AI::Categorize
   
   ... time passes ...
   
-  $nb = AI::Categorizer::Learner::KNN->restore_state('filename');
+  $l = AI::Categorizer::Learner->restore_state('filename');
   my $c = new AI::Categorizer::Collection::Files( path => ... );
   while (my $document = $c->next) {
-    my $hypothesis = $nb->categorize($document);
+    my $hypothesis = $l->categorize($document);
     print "Best assigned category: ", $hypothesis->best_category, "\n";
     print "All assigned categories: ", join(', ', $hypothesis->categories), "\n";
   }
 
 =head1 DESCRIPTION
 
-This is an implementation of the Naive Bayes decision-making
+This is an implementation of the k-Nearest-Neighbor decision-making
 algorithm, applied to the task of document categorization (as defined
 by the AI::Categorizer module).  See L<AI::Categorizer> for a complete
 description of the interface.
@@ -196,18 +162,24 @@ of its methods are available unless explicitly mentioned here.
 
 =head2 new()
 
-Creates a new Naive Bayes Learner and returns it.  In addition to the
+Creates a new KNN Learner and returns it.  In addition to the
 parameters accepted by the C<AI::Categorizer::Learner> class, the
-Naive Bayes subclass accepts the following parameters:
+KNN subclass accepts the following parameters:
 
 =over 4
 
-=item * threshold
+=item threshold
 
 Sets the score threshold for category membership.  The default is
-currently 0.3.  Set the threshold lower to assign more categories per
+currently 0.1.  Set the threshold lower to assign more categories per
 document, set it higher to assign fewer.  This can be an effective way
 to trade of between precision and recall.
+
+=item k_value
+
+Sets the C<k> value (as in k-Nearest-Neighbor) to the given integer.
+This indicates how many of each document's nearest neighbors should be
+considered when assigning categories.  The default is 5.
 
 =back
 
@@ -237,95 +209,17 @@ details on how to use this object.
 Saves the categorizer for later use.  This method is inherited from
 C<AI::Categorizer::Storable>.
 
-=head1 THEORY
-
-Bayes' Theorem is a way of inverting a conditional probability. It
-states:
-
-                P(y|x) P(x)
-      P(x|y) = -------------
-                   P(y)
-
-The notation C<P(x|y)> means "the probability of C<x> given C<y>."  See also
-L<"http://forum.swarthmore.edu/dr.math/problems/battisfore.03.22.99.html">
-for a simple but complete example of Bayes' Theorem.
-
-In this case, we want to know the probability of a given category given a
-certain string of words in a document, so we have:
-
-                    P(words | cat) P(cat)
-  P(cat | words) = --------------------
-                           P(words)
-
-We have applied Bayes' Theorem because C<P(cat | words)> is a difficult
-quantity to compute directly, but C<P(words | cat)> and C<P(cat)> are accessible
-(see below).
-
-The greater the expression above, the greater the probability that the given
-document belongs to the given category.  So we want to find the maximum
-value.  We write this as
-
-                                 P(words | cat) P(cat)
-  Best category =   ArgMax      -----------------------
-                   cat in cats          P(words)
-
-
-Since C<P(words)> doesn't change over the range of categories, we can get rid
-of it.  That's good, because we didn't want to have to compute these values
-anyway.  So our new formula is:
-
-  Best category =   ArgMax      P(words | cat) P(cat)
-                   cat in cats
-
-Finally, we note that if C<w1, w2, ... wn> are the words in the document,
-then this expression is equivalent to:
-
-  Best category =   ArgMax      P(w1|cat)*P(w2|cat)*...*P(wn|cat)*P(cat)
-                   cat in cats
-
-That's the formula I use in my document categorization code.  The last
-step is the only non-rigorous one in the derivation, and this is the
-"naive" part of the Naive Bayes technique.  It assumes that the
-probability of each word appearing in a document is unaffected by the
-presence or absence of each other word in the document.  We assume
-this even though we know this isn't true: for example, the word
-"iodized" is far more likely to appear in a document that contains the
-word "salt" than it is to appear in a document that contains the word
-"subroutine".  Luckily, as it turns out, making this assumption even
-when it isn't true may have little effect on our results, as the
-following paper by Pedro Domingos argues:
-L<"http://www.cs.washington.edu/homes/pedrod/mlj97.ps.gz">
-
-=head1 CALCULATIONS
-
-The various probabilities used in the above calculations are found
-directly from the training documents.  For instance, if there are 5000
-total tokens (words) in the "sports" training documents and 200 of
-them are the word "curling", then C<P(curling|sports) = 200/5000 =
-0.04> .  If there are 10,000 total tokens in the training corpus and
-5,000 of them are in documents belonging to the category "sports",
-then C<P(sports)> = 5,000/10,000 = 0.5> .
-
-Because the probabilities involved are often very small and we
-multiply many of them together, the result is often a tiny tiny
-number.  This could pose problems of floating-point underflow, so
-instead of working with the actual probabilities we work with the
-logarithms of the probabilities.  This also speeds up various
-calculations in the C<categorize()> method.
-
-=head1 TO DO
-
-More work on the confidence scores - right now the winning category
-tends to dominate the scores overwhelmingly, when the scores should
-probably be more evenly distributed.
-
 =head1 AUTHOR
 
-Ken Williams, ken@forum.swarthmore.edu
+Originally written by David Bell (C<< <dave@student.usyd.edu.au> >>),
+October 2002.
+
+Added to AI::Categorizer November 2002, modified, and maintained by
+Ken Williams (C<< <ken@mathforum.org> >>).
 
 =head1 COPYRIGHT
 
-Copyright 2000-2001 Ken Williams.  All rights reserved.
+Copyright 2000-2002 Ken Williams.  All rights reserved.
 
 This library is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
@@ -336,12 +230,5 @@ AI::Categorize(3)
 
 "A re-examination of text categorization methods" by Yiming Yang
 L<http://www.cs.cmu.edu/~yiming/publications.html>
-
-"On the Optimality of the Simple Bayesian Classifier under Zero-One
-Loss" by Pedro Domingos
-L<"http://www.cs.washington.edu/homes/pedrod/mlj97.ps.gz">
-
-A simple but complete example of Bayes' Theorem from Dr. Math
-L<"http://www.mathforum.com/dr.math/problems/battisfore.03.22.99.html">
 
 =cut

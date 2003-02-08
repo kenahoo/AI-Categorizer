3a36bd1ef89041ef471b0b23c4e5074a69db68b6
kwilliams <kwilliams@a22099ce-342d-0410-a4cf-3bf2e08833ce>
2003-02-08 17:01:25 +0000

    Added to CVS

---
+++ AI-Categorizer/lib/AI/Categorizer/FeatureVector/FastDot.pm
@@ -0,0 +1,105 @@
+package AI::Categorizer::FeatureVector::FastDot;
+
+use strict;
+use AI::Categorizer::FeatureVector;
+use vars qw($VERSION @ISA);
+use DynaLoader ();
+
+$VERSION = '0.01';
+@ISA = qw(DynaLoader AI::Categorizer::FeatureVector);
+bootstrap AI::Categorizer::FeatureVector::FastDot $VERSION;
+
+
+my %all_features;
+
+sub all_features {
+  my $pkg = shift;
+  if (@_) {
+    die "all_features() already set" if keys %all_features;
+    my $i = 0;
+    %all_features = map {$_, $i++} @{$_[0]};
+  }
+  return \%all_features;
+}
+
+sub dot {
+  my ($self, $other) = @_;
+  return $self->SUPER::dot($other) unless UNIVERSAL::isa($other, __PACKAGE__) && keys %all_features;
+  $self->_make_sparse_vec unless $self->{c_array};
+  $other->_make_sparse_vec unless $other->{c_array};
+  return _dot($self->{c_array}, $other->{c_array});
+}
+
+sub _make_sparse_vec {
+  my $self = shift;
+  my @names = sort {$all_features{$a} <=> $all_features{$b}} $self->names;
+  my @indices = @all_features{ @names };
+  my @values = $self->values( @names );
+  $self->{c_array} = _make_array(\@indices, \@values);
+}
+
+1;
+__END__
+
+=head1 NAME
+
+AI::Categorizer::FeatureVector - Features vs. Values
+
+=head1 SYNOPSIS
+
+  my $f1 = new AI::Categorizer::FeatureVector
+    (features => {howdy => 2, doody => 3});
+  my $f2 = new AI::Categorizer::FeatureVector
+    (features => {doody => 1, whopper => 2});
+   
+  @names = $f1->names;
+  $x = $f1->length;
+  $x = $f1->sum;
+  $x = $f1->includes('howdy');
+  $x = $f1->value('howdy');
+  $x = $f1->dot($f2);
+  
+  $f3 = $f1->clone;
+  $f3 = $f1->intersection($f2);
+  $f3 = $f1->add($f2);
+  
+  $h = $f1->as_hash;
+  $h = $f1->as_boolean_hash;
+  
+  $f1->normalize;
+
+=head1 DESCRIPTION
+
+This class implements a "feature vector", which is a flat data
+structure indicating the values associated with a set of features.  At
+its base level, a FeatureVector usually represents the set of words in
+a document, with the value for each feature indicating the number of
+times each word appears in the document.  However, the values are
+arbitrary so they can represent other quantities as well, and
+FeatureVectors may also be combined to represent the features of
+multiple documents.
+
+=head1 METHODS
+
+=over 4
+
+=item ...
+
+=back
+
+=head1 AUTHOR
+
+Ken Williams, ken@mathforum.org
+
+=head1 COPYRIGHT
+
+Copyright 2000-2002 Ken Williams.  All rights reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+AI::Categorizer(3), Storable(3)
+
+=cut
---
+++ AI-Categorizer/lib/AI/Categorizer/FeatureVector/FastDot.xs
@@ -0,0 +1,104 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+#ifdef __cplusplus
+}
+#endif
+
+typedef struct {
+  int length;  /* How many entries in this vector */
+  long *indices;  /* Indices of vector entries */
+  float *values;  /* Values of vector entries */
+} sparse_vec;
+
+#define debug 0
+
+
+MODULE = AI::Categorizer::FeatureVector::FastDot         PACKAGE = AI::Categorizer::FeatureVector::FastDot
+
+PROTOTYPES: DISABLE
+
+SV *
+_make_array(indices_r, values_r)
+    SV *indices_r
+    SV *values_r
+  CODE:
+    {
+      AV *indices, *values;
+      SV *output;
+      sparse_vec *vec;
+      I32 i;
+      
+      if (debug) warn("starting\n");
+      if (!SvROK(indices_r) || (SvTYPE(SvRV(indices_r)) != SVt_PVAV)) {
+	croak("first parameter to _make_array() was not an array reference");
+      }
+      if (!SvROK(values_r)  || (SvTYPE(SvRV(values_r))  != SVt_PVAV)) {
+	croak("second parameter to _make_array() was not an array reference");
+      }
+      
+      if (debug) warn("Checked input\n");
+      indices = (AV*) SvRV(indices_r);
+      values = (AV*) SvRV(values_r);
+      New(0, vec, 1, sparse_vec);
+      
+      if (debug) warn("Created struct\n");
+      vec->length = av_len(indices) + 1;
+      if (av_len(values) + 1 != vec->length) {
+	croak("array lengths don't match");
+      }
+      New(0, vec->indices, vec->length, long);
+      New(0, vec->values,  vec->length, float);
+      
+      for (i=0; i<vec->length; i++) {
+	vec->indices[i] = SvIV(*av_fetch(indices, i, 0));
+	vec->values[i]  = (float) SvNV(*av_fetch(values,  i, 0));
+      }
+      
+      /* Wrap in an SV */
+      if (debug) warn("wrapping in SV\n");
+      output = newSViv((IV)vec);
+      if (debug) warn("wrapped in SV\n");
+      RETVAL = output;
+    }
+  OUTPUT:
+    RETVAL
+
+
+double
+_dot(v1_r, v2_r)
+    SV *v1_r
+    SV *v2_r
+  CODE:
+    {
+      float sum = 0;
+      int i1 = 0, i2 = 0;
+      sparse_vec *v1 = (sparse_vec *)SvIV(v1_r);
+      sparse_vec *v2 = (sparse_vec *)SvIV(v2_r);
+
+      while (1) {
+	if (debug) warn("Beginning dot-loop\n");
+	if (i1 == v1->length || i2 == v2->length) break;
+	
+	if (v1->indices[i1] < v2->indices[i2]) {
+	  i1++;
+	  continue;
+	}
+	if (v1->indices[i1] > v2->indices[i2]) {
+	  i2++;
+	  continue;
+	}
+	
+	/* Indices here are equal, so add to sum */
+	sum += v1->values[i1] * v2->values[i2];
+	i1++;
+	i2++;
+      }
+      
+      RETVAL = (double) sum;
+    }
+  OUTPUT:
+    RETVAL
